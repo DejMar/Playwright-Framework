6086767fc03526d7852119b8ba1bbbd381f4b73c
DejMar <marjanovic.dejan@gmail.com>
2023-08-21 18:41:10 +0200

    Update e2e tests

--- tests/e2e/e2e-submit-form.spec.ts
+++ tests/e2e/e2e-submit-form.spec.ts
@@ -2,31 +2,31 @@ import { test, expect } from '@playwright/test'
 
 test.describe('Feedback Form', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://zero.webappsecurity.com/index.html')
-    await page.click('#feedback')
+    await page.goto('http://zero.webappsecurity.com/index.html');
+    await page.click('#feedback');
   })
 
   // Reset feedback form
   test('Reset feedback form', async ({ page }) => {
-    await page.type('#name', 'some name')
-    await page.type('#email', 'some email@email.com')
-    await page.type('#subject', 'some subject')
-    await page.type('#comment', 'some nice comment about the application')
-    await page.click("input[name='clear']")
+    await page.type('#name', 'some name');
+    await page.type('#email', 'some email@email.com');
+    await page.type('#subject', 'some subject');
+    await page.type('#comment', 'some nice comment about the application');
+    await page.click("input[name='clear']");
 
-    const nameInput = await page.locator('#name')
-    const commentInput = await page.locator('#comment')
-    await expect(nameInput).toBeEmpty()
-    await expect(commentInput).toBeEmpty()
+    const nameInput = await page.locator('#name');
+    const commentInput = await page.locator('#comment');
+    await expect(nameInput).toBeEmpty();
+    await expect(commentInput).toBeEmpty();
   })
 
   // Submit feedback form
   test('Submit feedback form', async ({ page }) => {
-    await page.type('#name', 'some name')
-    await page.type('#email', 'some email@email.com')
-    await page.type('#subject', 'some subject')
-    await page.type('#comment', 'some nice comment about the application')
-    await page.click("input[type='submit']")
-    await page.waitForSelector('#feedback-title')
+    await page.type('#name', 'some name');
+    await page.type('#email', 'some email@email.com');
+    await page.type('#subject', 'some subject');
+    await page.type('#comment', 'some nice comment about the application');
+    await page.click("input[type='submit']");
+    await page.waitForSelector('#feedback-title');
   })
 })
